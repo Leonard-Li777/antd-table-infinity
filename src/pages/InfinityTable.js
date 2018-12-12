d479e49f36dce40dc07dc150be77041cdcc853ea
lilun <lilun_cd@keruyun.com>
2018-12-12 14:49:04 +0800

    fix: umi auto router

--- src/pages/InfinityTable.js
+++ src/pages/InfinityTable.js
@@ -1,16 +1,13 @@
-import { Spin } from 'antd';
-import { SumTable as Table } from 'antd-table-infinity';
+import { InfinityTable as Table } from 'antd-table-infinity';
 import React, { Component } from 'react';
 
-import 'antd-table-infinity/dist/index.css';
-import 'antd-table-infinity/index.css';
-
-import { columns, fetchData, sumData } from '../stories/Table/mockData';
-
+import { columns, fetchData } from '../stories/Table/mockData';
 
 // import { SumTable as Table } from '../components/Table';
-// import { SumTable as Table } from '../dist/index';
-// import '../components/Table/index.less';
+// import '../components/Table/SumTable/index.css';
+
+// import { InfinityTable as Table } from '../dist/index';
+// import '../dist/index.css';
 
 class App extends Component {
   state = {
@@ -28,19 +25,6 @@ class App extends Component {
     );
   };
 
-  loadMoreContent = () => (
-    <div
-      style={{
-        textAlign: 'center',
-        paddingTop: 40,
-        paddingBottom: 40,
-        border: '1px solid #e8e8e8',
-      }}
-    >
-      <Spin tip="Loading..." />
-    </div>
-  );
-
   render() {
     return (
       <Table
@@ -48,9 +32,7 @@ class App extends Component {
         loading={this.state.loading}
         onFetch={this.handleFetch}
         pageSize={50}
-        loadingIndicator={this.loadMoreContent()}
         columns={columns}
-        sumData={sumData}
         scroll={{ y: 450 }}
         dataSource={this.state.data}
         bordered
